8dcf9f3f928930ea89bdaabd2ae806ee592c292f
Erik Torres <erik.torres@codurance.com>
2017-12-17 21:12:08 +0100

    used open-override to define user name in commands

--- src/main/kotlin/es/eriktorr/katas/command/Command.kt
+++ src/main/kotlin/es/eriktorr/katas/command/Command.kt
@@ -1,13 +1,13 @@
 package es.eriktorr.katas.command
 
-sealed class Command(private val userName: String) {
+sealed class Command(open val userName: String) {
 
-    data class PostingCommand(val userName: String, val message: String): Command(userName)
+    data class PostingCommand(override val userName: String, val message: String): Command(userName)
 
-    data class ReadingCommand(val userName: String): Command(userName)
+    data class ReadingCommand(override val userName: String): Command(userName)
 
-    data class FollowingCommand(val userName: String, val followedUserName: String): Command(userName)
+    data class FollowingCommand(override val userName: String, val followedUserName: String): Command(userName)
 
-    data class WallCommand(val userName: String): Command(userName)
+    data class WallCommand(override val userName: String): Command(userName)
 
 }
